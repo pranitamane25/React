e61cf63c46383ef4d6308f3850a12fa76707f637
pranitamane25 <pranitamane2506@gmail.com>
2026-01-13 10:55:51 +0530

    First React application

---
+++ ReactFirstApp/my-vite-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <div>My First React app</div>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
